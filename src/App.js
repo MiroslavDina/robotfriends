53f554a35812491fd535d81a5b7afa98f2a97dd0
MiroslavDina <miroslav.dina86@gmail.com>
2021-10-05 23:12:51 +0200

    ..

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import CardList from './CardList';
-import SearchBox from '/SearchBox';
+import SearchBox from './SearchBox';
 import { robots } from './robots';
 
 const App = () => {
 	return (
 		<div className='tc'>
 			<h1>RoboFrineds</h1>
-			<searchBox />
+			<SearchBox />
 			<CardList robots={robots}/>
 		</div>
 	);
