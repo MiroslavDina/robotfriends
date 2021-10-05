2290be34760f51a9d415ae9aeff3aaeb7bb94867
MiroslavDina <miroslav.dina86@gmail.com>
2021-10-05 21:36:26 +0200

    significant changes in project

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import Card from './Card';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
@@ -12,7 +12,7 @@ import 'tachyons';
 //   document.getElementById('root')
 // );
 
-ReactDOM.render(<Hello />, document.getElementById('root'))
+ReactDOM.render(<Card />, document.getElementById('root'))
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
